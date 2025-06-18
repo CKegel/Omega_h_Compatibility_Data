e1b19b281bd3bc173a66ae783fa1f36d5fcef0f3
CKegel <57967583+CKegel@users.noreply.github.com>
2025-06-18 16:42:10 -0400

    Add column and row labels to processing script.

--- processDeltaWing.sh
+++ processDeltaWing.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-csplit -z $1 /Branch/ '{*}'
-FirstRunTiming=`grep seconds xx00 | sort | grep -E '[0-9][\.][0-9]+' -o`
-SecondRunTiming=`grep seconds xx01 | sort | grep -E '[0-9][\.][0-9]+' -o | tr ' ' '\n'`
+csplit -z -s $1 /Branch/ '{*}'
+FirstRunTiming=`grep seconds xx00 | sed -E 's/ took /,/' | sed 's/ seconds//'`
+SecondRunTiming=`grep seconds xx01 | grep -E '[0-9][\.][0-9]+' -o | tr ' ' '\n'`
 
-PastedOutput=`paste <(echo "$FirstRunTiming") <(echo "$SecondRunTiming") --delimiters ', '`
+PastedOutput=`paste <(printf "$FirstRunTiming") <(printf "$SecondRunTiming") --delimiters ','`
+echo "Operation,FirstRunTiming,SecondRunTiming,"
 echo "$PastedOutput"
